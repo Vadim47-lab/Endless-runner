475eb8379abcea0ef20990966c8f1e41e269ae3b
Stepanov Vadim <vadgat777@mail.ru>
2022-01-19 18:13:28 +0700

    seven commit

--- Scripts/Player/Player.cs
+++ Scripts/Player/Player.cs
@@ -1,12 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private UnityEvent _encounterEnemy;
     [SerializeField] private int _health;
+
+    public event UnityAction<int> HealthChanged;
+
+    private void Start()
+    {
+        HealthChanged?.Invoke(_health);
+    }
 
     public void ApplyDamage(int damage)
     {
         _health -= damage;
+        HealthChanged?.Invoke(_health);
+        _encounterEnemy?.Invoke();
 
         if (_health <= 0)
         {
